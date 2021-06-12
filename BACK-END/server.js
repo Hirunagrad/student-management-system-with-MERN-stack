f76add323a9c036d876fe3fc098fa0978da1a408
Hirunagrad <hirunagrad@gmail.com>
2021-06-13 03:09:15 +0530

    create connection with database

---
+++ BACK-END/server.js
@@ -0,0 +1,52 @@
+
+// get all backend dependencies to server js
+
+const express = require("express");
+const mongoose=require("mongoose");
+const bodyParser =require("body-parser");
+const cors =require("cors");
+const app = express();
+require("dotenv").config();
+
+//define the port
+//process.env.PORT =  run the code any port available
+
+const PORT  = process.env.PORT || 8070;
+
+//use cors and bodyparser
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//conect the url in .env file
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+
+   useCreateIndex: true,
+   useNewUrlParser: true,
+   useUnifiedTopology : true,
+   useFindAndModify : false
+    
+
+});
+
+
+//create connection
+
+const connection = mongoose.connection;
+
+//open=connection open
+connection.once('open', () => {
+
+  console.log("MongoDB connection Sucsess!");
+
+})
+
+//all are ready listen the port
+app.listen(PORT, () => {
+
+  console.log(`Server up and running on port number: ${PORT}`);
+
+})
